d51225d31a8af38702a3d916bffa0a7ea3c840ef
Vincent Chan <vincent@vincentcchan.com>
2014-03-10 15:41:18 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var User = require('../models/User');
+
+exports.collection = function(req, res){
+  res.setHeader('Content-type', 'application/json');
+  res.send(JSON.stringify({msg: 'hello world!'}));
+  User.find({}, function(err, users){
+    if(err){
+      res.writeHead(500);
+      res.send({'error': err});
+    }else{
+      res.send(users);
+    }
+  });
+};
+
+exports.createUser = function(req, res){
+  res.setHeader('Content-type', 'application/json');
+  var user = new User(req.body);
+  user.save(function(err, resUser){
+    if(err){
+      res.writeHead(500);
+      res.send({'error':err});
+    }else{
+      res.send(resUser);
+    }
+  });
+};

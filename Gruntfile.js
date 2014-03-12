7674dfb9c57c6a8ae6f314d1c3217750d5d93045
Vincent Chan <vincent@vincentcchan.com>
2014-03-11 21:53:24 -0700

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,35 +8,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-casper');
 
   grunt.initConfig({
-    simplemocha:{
-      dev:{
-        src:['test/*_test.js'],
-        options:{
-          reporter: 'spec',
-          slow: 200,
-          timeout: 1000
-        }
-      }
-    },
-    watch:{
-      all:{
-        files:['app.js', 'models/*.js'],
-        tasks:['jshint', 'test']
-      }
-    },
-    jshint: {
-      all: ['Gruntfile.js', 'app.js', 'models/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: true,
-        globals: {
-          console: true,
-          module: true
-        }
-      }
-    },
     express: {
       options: {
-        //
+        // Override defaults here
       },
       dev: {
         options: {
@@ -55,41 +29,56 @@ module.exports = function(grunt) {
         }
       }
     },
-
-    browserify: {
-      dev: {
-        src: ['assets/js/*.js'],
-        dest: 'build/browser.js',
-        options: {
-          transform: ['debowerify'],
-          debug: true
+    simplemocha: {
+      dev:{
+        src:['test/*_test.js','!test/acceptance/*_test.js'],
+        options:{
+          reporter: 'spec',
+          slow: 200,
+          timeout: 1000
         }
-      },
-      prod: {
-
       }
     },
-
-    bower: {
-
-    },
-
-    casper: {
-      acceptance: {
+    watch: {
+      all: {
+        files:['app.js', '**/*.js' ],
+        tasks:['jshint']
+      },
+      express: {
+        files:  [ 'app.js','models/**/*.js','routes/**/*.js' ],
+        tasks:  [ 'express:dev' ],
         options: {
-          test: true
+          // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions.
+          // Without this option specified express won't be reloaded
+          spawn: false
+        }
+      }
+    },
+    casper: {
+      acceptance : {
+        options : {
+          test : true,
         },
-        files: {
+        files : {
           'test/acceptance/casper-results.xml' : ['test/acceptance/*_test.js']
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'app.js', 'models/**/*.js', 'test/**/*.js'],
+      options: {
+        jshintrc: true,
+        globals: {
+          console: true,
+          module: true
         }
       }
     }
   });
 
-
-  grunt.registerTask('build:dev')
-  grunt.registerTask('test', 'simplemocha:dev');
-  grunt.registerTask('watch', 'test, watch:all');
-  grunt.registerTask('test:acceptance', ['express:dev', 'casper'])
+  grunt.registerTask('test', ['jshint', 'simplemocha:dev']);
+  grunt.registerTask('server', [ 'jshint', 'express:dev','watch:express' ]);
+  grunt.registerTask('test:acceptance',['express:dev','casper']);
+  grunt.registerTask('default', ['jshint', 'test','watch:express']);
 
 };
